aa0da8631414ef34e30689c7dc89ef03a6482a2e
NOI <aa8139106@gmail.com>
2022-04-28 23:21:14 +0800

    add dist

---
+++ dist/js/837.08ce2761.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunke_commerce"]=self["webpackChunke_commerce"]||[]).push([[837],{4837:function(e,r,t){t.r(r),t.d(r,{default:function(){return i}});var u=t(3396);const c=(0,u.Uk)("產品列表");function n(e,r,t,n,s,o){const a=(0,u.up)("router-link");return(0,u.wg)(),(0,u.j4)(a,{to:"/user/list"},{default:(0,u.w5)((()=>[c])),_:1})}var s={created(){this.$router.replace("/user/list")}},o=t(89);const a=(0,o.Z)(s,[["render",n]]);var i=a}}]);
+//# sourceMappingURL=837.08ce2761.js.map
