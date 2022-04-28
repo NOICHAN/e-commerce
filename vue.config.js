ed8331b7003c434bd320da4ec9199e2df1dd2a51
NOI <aa8139106@gmail.com>
2022-04-29 00:54:12 +0800

    add

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  // publicPath: '/dist',
   publicPath: process.env.NODE_ENV === 'production'
     ? '/e-commerce'
     : '/',
