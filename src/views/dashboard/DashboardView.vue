846a6ecdfe85736333221d117ba5aa009b39532f
NOI <aa8139106@gmail.com>
2022-05-03 18:45:23 +0800

    Revise

--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -28,10 +28,10 @@ export default {
     // token 加到 Headers 裡面
     this.$http.defaults.headers.common.Authorization = token;
     const api = `${process.env.VUE_APP_API}api/user/check`;
-    this.$http.post(api, this.user)
+    this.$http.post(api)
       .then((res) => {
         if (!res.data.success) {
-          this.$router.push('/login');
+          this.$router.push('/user/login');
         }
       });
   },
